fbc04a9e0c7b3aceff8f9807608840bcd0d27be0
alpcanaydin <alpcan@alpcanaydin.com>
2016-12-15 16:43:31 +0300

    Github pages created

--- src/actions/microphone.js
+++
@@ -1,6 +0,0 @@
-export const MICROPHONE_STATUS_CHANGED = 'MICROPHONE_STATUS_CHANGED';
-
-export const microphoneStatusChanged = status => ({
-  type: MICROPHONE_STATUS_CHANGED,
-  status
-});
--- src/actions/status.js
+++
@@ -1,6 +0,0 @@
-export const GAME_STATUS_CHANGED = 'GAME_STATUS_CHANGED';
-
-export const gameStatusChanged = status => ({
-  type: GAME_STATUS_CHANGED,
-  status
-});
